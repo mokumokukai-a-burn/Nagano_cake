25b8544170308efe99ac0e2a580a91278982454f
yuta-iiduka <ota12041204@gmail.com>
2021-02-21 04:36:26 +0000

    ブートストラップでデザインを調整

--- db/schema.rb
+++ db/schema.rb
@@ -34,12 +34,31 @@ ActiveRecord::Schema.define(version: 2021_02_19_133656) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "customer_id"
     t.integer "product_id"
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "costomers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "street_address", null: false
+    t.string "post_number", null: false
+    t.string "phone_number", null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_costomers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_costomers_on_reset_password_token", unique: true
   end
 
   create_table "customers", force: :cascade do |t|
