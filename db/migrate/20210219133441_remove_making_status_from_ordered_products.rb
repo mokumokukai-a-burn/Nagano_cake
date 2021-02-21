4a49456f90d8138399497b530d255f7d08bd19b4
milan2229 <milan_1899.22@icloud.com>
2021-02-21 10:30:37 +0000

    注文完了まで終了

--- db/migrate/20210219133441_remove_making_status_from_ordered_products.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMakingStatusFromOrderedProducts < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :ordered_products, :making_status, :integer
-  end
-end
--- db/migrate/20210219133656_add_making_status_to_ordered_products.rb
+++
@@ -1,5 +0,0 @@
-class AddMakingStatusToOrderedProducts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :ordered_products, :making_status, :integer, null: false, default:0
-  end
-end
