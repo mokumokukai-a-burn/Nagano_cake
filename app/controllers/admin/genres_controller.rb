dbec906fcc1e58a657a73d5f0595be8019627ed6
EC2 Default User <ec2-user@ip-172-31-47-116.ec2.internal>
2021-02-22 02:11:01 +0000

    finish genre adress

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -15,9 +15,9 @@ class Admin::GenresController < ApplicationController
   end
   
   def update
-    genre = Genre.find(params[:id])
-    genre.update(genre_params)
-    redirect_to new_genre_path
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to new_admin_genre_path
   end
 
   private
