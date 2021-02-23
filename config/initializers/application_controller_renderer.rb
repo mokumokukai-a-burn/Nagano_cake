de57a5973768d0a3ea7ef08e382e6f83aa4fa389
haranokengo <haranokengo@gmail.com>
2021-02-23 06:43:10 +0000

    原野Finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '12e916689d24b6eeb6fff0a8415c7e1c7f3cfb2beb1fb329d2c8b90efe7200c4e0cff7f74639322244c5651d1e6ee0c010e69d4d3cdabba724910c6938d7e440'
