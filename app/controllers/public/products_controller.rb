4df1ef12e0e9c75d17e637639a513981d25131a6
haranokengo <haranokengo@gmail.com>
2021-02-24 16:35:51 +0000

    A

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,5 +1,5 @@
 class Public::ProductsController < ApplicationController
-  before_action :authenticate_customer!, except: [:index, :show]
+  before_action :authenticate_customer!, except: [:index]
 
   def index
     @products = Product.where(is_active: 'true').page(params[:page]).per(8)
